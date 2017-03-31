ecf2139718e2fa7500090fb30d333fc60dac1bbb
Reilly Kearney <Reilly@reillys-air.dhcp.nd.edu>
2017-03-31 13:56:48 -0400

    Settings and New Update in Progress

---
+++ app/src/main/java/com/a40333/bharrin4/triangle_care/NewUpdateActivity.java
@@ -0,0 +1,13 @@
+package com.a40333.bharrin4.triangle_care;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewUpdateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_update);
+    }
+}
